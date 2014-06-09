84b7d0959ddfa90f59cc07ab3caf5d247539ada6
Christophe Philemotte <christophe.philemotte@8thcolor.com>
2014-06-09 10:23:45 +0200

    first invoicing: some links + idea of failed functionality (total)

---
+++ lib/invoice.rb
@@ -0,0 +1,32 @@
+class Invoice
+  def initialize
+    @items = []
+    @total = 0
+  end
+
+  def add_item(item)
+    @items << item
+  end
+
+  def calculate_total
+    @total = 0 # test fail if forgotten
+    @items.each do |item|
+      @total += item.price * item.quantity
+    end
+    @total
+  end
+
+# 1- Rabais
+# 2- Remise
+# 3- Escompte
+# 4- Frais d'emballage
+# 5- Frais de port
+# VAT
+
+# http://en.wikipedia.org/wiki/Discounts_and_allowances
+# http://en.wikipedia.org/wiki/Rebate_%28marketing%29
+# http://en.wikipedia.org/wiki/Goods_and_Services_Tax_%28Canada%29
+# http://en.wikipedia.org/wiki/Sales_tax
+# http://en.wikipedia.org/wiki/Invoice
+
+end
